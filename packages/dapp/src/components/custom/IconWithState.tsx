9e02aea22fa8a7e92bffca8b44fe885fb8f6fc10
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2021-12-10 17:12:09 +0530

    Add form stepper boilerplate

--- packages/dapp/src/components/custom/IconWithState.tsx
+++ packages/dapp/src/components/custom/IconWithState.tsx
@@ -15,7 +15,8 @@ import {
   useDisclosure,
   VStack,
 } from "@chakra-ui/react";
-import { useForm } from "react-hook-form";
+import { Step, Steps, useSteps } from "chakra-ui-steps";
+import { FormProvider, useForm } from "react-hook-form";
 import { SiDiscord, SiGitbook, SiGithub, SiTwitter } from "react-icons/si";
 
 type IconWithStateProps = {
@@ -37,6 +38,49 @@ function IconWithState({ icon, active = false }: IconWithStateProps) {
     console.log(values);
   }
 
+  const { nextStep, prevStep, activeStep } = useSteps({
+    initialStep: 0,
+  });
+
+  const UsernameForm = () => {
+    return <FormControl isInvalid={errors.name}>
+    <FormLabel htmlFor="name">Username</FormLabel>
+    <Input
+      placeholder="Project name"
+      {...register("name", {
+        required: "This is required",
+        maxLength: {
+          value: 150,
+          message: "Maximum length should be 150",
+        },
+      })}
+    />
+    <FormErrorMessage>
+      {errors.name && errors.name.message}
+    </FormErrorMessage>
+  </FormControl>
+  }
+
+  const steps = [
+    {
+      label: "Step 1",
+      content: UsernameForm,
+    },
+  ];
+
+  const methods = useForm({
+    defaultValues: {
+      logo: null,
+      squads: [
+        {
+          name: "Genesis",
+          members: ["0x0000000000000"],
+          image: null,
+        },
+      ],
+    },
+  });
+
   let SiIcon;
   switch (icon) {
     case "discord":
@@ -55,7 +99,7 @@ function IconWithState({ icon, active = false }: IconWithStateProps) {
       SiIcon = SiDiscord;
   }
   return (
-    <>
+    <FormProvider {...methods}>
       <IconButton
         variant="unstyled"
         aria-label="Discord"
@@ -72,23 +116,15 @@ function IconWithState({ icon, active = false }: IconWithStateProps) {
           <ModalHeader>Discord</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
+            <Steps colorScheme="purple" activeStep={activeStep}>
+              {steps.map(({ label, content }) => (
+                <Step label={label} key={label}>
+                  {content}
+                </Step>
+              ))}
+            </Steps>
             <VStack w="full" onSubmit={handleSubmit(onSubmit)}>
-              <FormControl isInvalid={errors.name}>
-                <FormLabel htmlFor="name">Username</FormLabel>
-                <Input
-                  placeholder="Project name"
-                  {...register("name", {
-                    required: "This is required",
-                    maxLength: {
-                      value: 150,
-                      message: "Maximum length should be 150",
-                    },
-                  })}
-                />
-                <FormErrorMessage>
-                  {errors.name && errors.name.message}
-                </FormErrorMessage>
-              </FormControl>
+              <UsernameForm />
             </VStack>
           </ModalBody>
           <ModalFooter alignSelf="center">
@@ -103,7 +139,7 @@ function IconWithState({ icon, active = false }: IconWithStateProps) {
           </ModalFooter>
         </ModalContent>
       </Modal>
-    </>
+    </FormProvider>
   );
 }
 
